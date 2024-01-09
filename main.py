ff8265e72f13ad57ca5ced568f9bd62c9ee7f21e
Saulius Anusas <saulius.anusas@gmail.com>
2024-01-09 22:01:20 +0200

    Isprestos 4 uzduotys

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,14 +13,53 @@ def calculation():
     return render_template("calculation.html")
 
 
-@app.route("/<name>")
-def home(name):
-    return f"<h1>Čia {name} naujas puslapis</h1>"
+@app.route("/keliamieji")
+def home():
+    start = 1900
+    end = 2100
+    leap_year = []
+    while start < end:
+        if start % 4 == 0 and start % 100 != 0:
+            leap_year.append(start)
+        if start % 100 == 0 and start % 400 == 0:
+            leap_year.append(start)
+        start += 1
+
+    return render_template(
+        "keliamieji.html", keliamieji=leap_year, start=1900, end=2100
+    )
+
+
+@app.route("/tikrinti", methods=["GET", "POST"])
+def check_year():
+    if request.method == "POST":
+        year_1 = int(request.form["metai"])
+        if (year_1 % 4 == 0 and year_1 % 100 != 0) or (
+            year_1 % 100 == 0 and year_1 % 400 == 0
+        ):
+            return render_template("tikrinti.html", yes=f"This is leap year: {year_1}")
+        else:
+            return render_template(
+                "tikrinti.html", yes=f"This is not leap year: {year_1}"
+            )
+    else:
+        return render_template("tikrinti.html")
+
 
 @app.route("/names")
 def name():
-    vardai = ['Jonas', 'Antanas', 'Petras']
+    vardai = ["Jonas", "Antanas", "Petras"]
     return render_template("names.html", sarasas=vardai)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        vardas = request.form["vardas"]
+        return render_template("greetings.html", var=vardas)
+    else:
+        return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
